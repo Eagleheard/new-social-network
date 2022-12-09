e612e9f3bcc76c2a4fe9da0cb1df15a558634aac
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-12-09 07:21:03 +0300

    feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <ul>
+        <li>Home</li>
+        <li>News</li>
+      </ul>
+    </header>
+  );
+};
