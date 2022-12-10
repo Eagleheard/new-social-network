1e08b6c51ba7407bc1ffb9833cc1ead23c2a722b
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-12-10 14:01:31 +0300

    feat: add posts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header/index";
 export { Input } from "./Input/index";
 export { Button } from "./Button/index";
+export { Post } from "./Post/index";
--- src/screen/Content/index.tsx
+++ src/screen/Content/index.tsx
@@ -1,7 +1,36 @@
-import { Input } from "components";
 import React from "react";
 
+import { Input, Post } from "components";
+
 import "./styles.scss";
+
+const initialStore = [
+  {
+    photo: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+    nickname: "User1",
+    post: "Post1",
+  },
+  {
+    photo: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+    nickname: "User2",
+    post: "Post2",
+  },
+  {
+    photo: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+    nickname: "User3",
+    post: "Post3",
+  },
+  {
+    photo: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+    nickname: "User4",
+    post: "Post4",
+  },
+  {
+    photo: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+    nickname: "User5",
+    post: "Post5",
+  },
+];
 
 export const Content = () => {
   return (
@@ -14,6 +43,9 @@ export const Content = () => {
         />
         <Input />
       </div>
+      {initialStore.map((el) => (
+        <Post {...el} />
+      ))}
     </div>
   );
 };
