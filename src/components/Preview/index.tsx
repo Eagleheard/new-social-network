d4032b53e218d7faa29a0ee1ba93f85d1334c248
Vladislav Krutikov <Eagleheard5@gmail.com>
2023-01-11 18:56:13 +0300

    feat: add structure of login page

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -10,6 +10,10 @@ export const Preview = () => {
       <div className="preview">
         <div className="preview__description">description</div>
         <div className="preview__login">
+          <h1 className="preview__welcome">Hello Again!</h1>
+          <p className="preview__welcome-description">
+            First of all you need to login!
+          </p>
           <SignIn
             handleSwitch={function (): void {
               throw new Error("Function not implemented.");
